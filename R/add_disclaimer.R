996096d8b04af3acf546765261b37c4565c9afc3
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-08-15 01:27:28 +0800

    give options for `license_name`

--- R/add_disclaimer.R
+++ R/add_disclaimer.R
@@ -1,15 +1,19 @@
 #' @export
-add_disclaimer <- function(name = "Jiaxiang Li") {
-  license_name <- which_license()
-  disclaimer_text <-
-    glue::glue(
-      '<h4 align="center">**Code of Conduct**</h4>
+add_disclaimer <-
+  function(name = "Jiaxiang Li",
+           license_name = FALSE) {
+    if (license_name = -FALSE) {
+      license_name <- which_license()
+    }
+    disclaimer_text <-
+      glue::glue(
+        '<h4 align="center">**Code of Conduct**</h4>
 
   <h6 align="center">Please note that the `{proj_name()}` project is released with a [Contributor Code of Conduct](.github/CODE_OF_CONDUCT.md).<br>By contributing to this project, you agree to abide by its terms.</h6>
 
   <h4 align="center">**License**</h4>
 
   <h6 align="center">{license_name} &copy; [{name}](LICENSE.md)</h6>'
-    )
-  return(disclaimer_text)
-}
+      )
+    return(disclaimer_text)
+  }
